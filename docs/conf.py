e37d44d5f3ba6ee7e051cb0a20c3bbfbbec7c7e5
wpk <william.krekelberg@nist.gov>
2023-12-21 17:01:51 -0500

    chore: update cruft

--- docs/conf.py
+++ docs/conf.py
@@ -125,7 +125,7 @@ nb_execution_allow_errors = True
 github_username = "usnistgov"
 
 html_context = {
-    "github_user": "usnistgov",
+    "github_user": github_username,
     "github_repo": "module-utilities",
     "github_version": "main",
     "doc_path": "docs",
@@ -257,8 +257,7 @@ author = "William P. Krekelberg"
 def _get_version():
     import os
 
-    version = os.environ.get("SETUPTOOLS_SCM_PRETEND_VERSION", None)
-    if version is None:
+    if (version := os.environ.get("SETUPTOOLS_SCM_PRETEND_VERSION")) is None:
         version = module_utilities.__version__
     return version
 

3bf375f46c022e3b07974e5c5f4e806fa073b20b
wpk-nist-gov <wpk@nist.gov>
2025-02-10 13:56:07 -0500

    chore: moved to using ruff.toml

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def add_standard_imports(doctest_namespace: dict[str, Any]) -> None:
+def add_standard_imports(doctest_namespace: dict[str, Any]) -> None:  # noqa: D103
     from module_utilities import cached
 
     doctest_namespace["cached"] = cached
